283e5870f15db5453b7fd4ab2d0b1aad8f14539f
Anatulya Nandi <anatulya.nandi@gmail.com>
2024-10-12 14:18:07 -0700

    removed extra eventlisting component, fixed view gallery/register button

--- src/app/components/EventListing.js
+++ src/app/components/EventListing.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export const EventListing2 = ({ name, date, link, city, fixit }) => {
+export const EventListing = ({ name, date, link, city, fixit }) => {
     const getFormattedDate = () => {
         return date.toLocaleString('default', { dateStyle: "short", timeStyle: "short" });
     }
@@ -9,6 +9,7 @@ export const EventListing2 = ({ name, date, link, city, fixit }) => {
         return Date.now() - date > 0
     }
 
+    console.log(date && dateHasPassed())
     return (
         <div className="flex flex-col sm:px-1 py-2.5 ">
             <div className="flex flex-row  sm:px-4 justify-between items-center sm:items-start text-base">
@@ -31,8 +32,8 @@ export const EventListing2 = ({ name, date, link, city, fixit }) => {
                 <div className="flex flex-col">
                     <div className="flex flex-row items-center sm:space-x-8">
                         <div className="flex flex-row items-center  space-x-1 ">
-                            <div className={`rounded-full w-2.5 h-2.5  flex-shrink-0 ${dateHasPassed() && date ? "bg-red-500" : "bg-green-500"}`}></div>
-                            <h1 className="text-white">{dateHasPassed() && date ? "Event Passed" : "Event Coming"}</h1>
+                            <div className={`rounded-full w-2.5 h-2.5  flex-shrink-0 ${date && dateHasPassed() ? "bg-red-500" : "bg-green-500"}`}></div>
+                            <h1 className="text-white">{date && dateHasPassed() ? "Event Passed" : "Event Coming"}</h1>
                         </div>
                     </div>
                     <Link href={link}><button disabled={link === ""} className="text-neutral-200 w-full px-1 py-0.5 bg-neutral-500 disabled:opacity-40 disabled:hover:cursor-not-allowed" >{dateHasPassed() && date ? "View Gallery" : "Register"}</button></Link>
--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from "react";
 import { EventListing } from "../components/EventListing";
-import { EventListing2 } from "../components/EventListing2";
 
 export default function EventPage() {
     const [openSCCLD, setOpenSCCLD] = useState(false)
@@ -9,25 +8,25 @@ export default function EventPage() {
     return (
         <div className="flex flex-col text-white">
             <h1 className="font-semibold">Upcoming Events</h1>
-            <EventListing2 name={"Sunnyvale Library"} date={new Date(2024, 11, 15, 14)} link={""} fixit={true} city={"Sunnyvale, CA"} />
-            <EventListing2 name={"Burlingame Library"} date={new Date(2025, 0, 5, 13, 30)} link={""} city={"Burlingame, CA"} />
-            <EventListing2 name={"Scotts Valley Library"} date={new Date(2025, 0, 4, 13)} link={""} city={"Santa Cruz, CA"} />
-            <EventListing2 name={"Cupertino Library"} date={new Date(2025, 2, 22, 14)} link={""} city={"Cupertino, CA"} />
-            <EventListing2 name={"THP South Branch Library"} date={new Date(2025, 5, 28, 14, 30)} link={""} city={"Berkeley, CA"} />
-            <EventListing2 name={"Fremont Main Library"} date={null} link={""} city={"Fremont, CA"} />
-            <EventListing2 name={"Dublin Library"} date={null} link={""} city={"Dublin, CA"} />
-            <EventListing2 name={"Union City Library"} date={null} link={""} city={"Union City, CA"} />
-            <EventListing2 name={"Redwood City Library"} date={null} link={""} city={"Redwood City, CA"} />
+            <EventListing name={"Sunnyvale Library"} date={new Date(2024, 11, 15, 14)} link={""} fixit={true} city={"Sunnyvale, CA"} />
+            <EventListing name={"Burlingame Library"} date={new Date(2025, 0, 5, 13, 30)} link={""} city={"Burlingame, CA"} />
+            <EventListing name={"Scotts Valley Library"} date={new Date(2025, 0, 4, 13)} link={""} city={"Santa Cruz, CA"} />
+            <EventListing name={"Cupertino Library"} date={new Date(2025, 2, 22, 14)} link={""} city={"Cupertino, CA"} />
+            <EventListing name={"THP South Branch Library"} date={new Date(2025, 5, 28, 14, 30)} link={""} city={"Berkeley, CA"} />
+            <EventListing name={"Fremont Main Library"} date={null} link={""} city={"Fremont, CA"} />
+            <EventListing name={"Dublin Library"} date={null} link={""} city={"Dublin, CA"} />
+            <EventListing name={"Union City Library"} date={null} link={""} city={"Union City, CA"} />
+            <EventListing name={"Redwood City Library"} date={null} link={""} city={"Redwood City, CA"} />
 
             <h1 className="mt-8 font-semibold">Past Events <span className="font-normal">(click to view gallery)</span></h1>
-            <EventListing2 name={"Sunnyvale Makerspace Nexus"} date={new Date(2024, 9, 5, 11)} link={"gallery/maker_nexus"} fixit={true} city={"Sunnyvale, CA"}/>
-            <EventListing2 name={"Burlingame Library"} date={new Date(2024, 8, 7, 13, 30)} link={"gallery/burlingame"} city={"Burlingame, CA"} />
-            <EventListing2 name={"Cupertino Library"} date={new Date(2024, 7, 17, 15)} link={"gallery/cupertino"} city={"Cupertino, CA"}  />
-            <EventListing2 name={"Morgan Hill Library"} date={new Date(2024, 7, 14, 18)} link={"gallery/morgan_hill"} city={"Morgan Hill, CA"} />
-            <EventListing2 name={"Gilroy Library"} date={new Date(2024, 6, 20, 13)} link={"gallery/gilroy"} city={"Gilroy, CA"} />
-            <EventListing2 name={"Los Altos Library"} date={new Date(2024, 6, 13, 10, 30)} link={"gallery/los_altos"} city={"Los Altos, CA"} />
-            <EventListing2 name={"Milpitas Library"} date={new Date(2024, 5, 22, 16)} link={"gallery/milpitas"}  city={"Milpitas, CA"} />
-            <EventListing2 name={"Saratoga Library"} date={new Date(2024, 5, 9, 13)} link={"gallery/saratoga"} city={"Saratoga, CA"}  />
+            <EventListing name={"Sunnyvale Makerspace Nexus"} date={new Date(2024, 9, 5, 11)} link={"gallery/maker_nexus"} fixit={true} city={"Sunnyvale, CA"}/>
+            <EventListing name={"Burlingame Library"} date={new Date(2024, 8, 7, 13, 30)} link={"gallery/burlingame"} city={"Burlingame, CA"} />
+            <EventListing name={"Cupertino Library"} date={new Date(2024, 7, 17, 15)} link={"gallery/cupertino"} city={"Cupertino, CA"}  />
+            <EventListing name={"Morgan Hill Library"} date={new Date(2024, 7, 14, 18)} link={"gallery/morgan_hill"} city={"Morgan Hill, CA"} />
+            <EventListing name={"Gilroy Library"} date={new Date(2024, 6, 20, 13)} link={"gallery/gilroy"} city={"Gilroy, CA"} />
+            <EventListing name={"Los Altos Library"} date={new Date(2024, 6, 13, 10, 30)} link={"gallery/los_altos"} city={"Los Altos, CA"} />
+            <EventListing name={"Milpitas Library"} date={new Date(2024, 5, 22, 16)} link={"gallery/milpitas"}  city={"Milpitas, CA"} />
+            <EventListing name={"Saratoga Library"} date={new Date(2024, 5, 9, 13)} link={"gallery/saratoga"} city={"Saratoga, CA"}  />
 
 
         </div>
