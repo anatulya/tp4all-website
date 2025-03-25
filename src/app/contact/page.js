0f395ad694b8fe99e37ed7ebfb56bfa1745e3817
Anatulya Nandi <anatulya.nandi@gmail.com>
2025-03-24 20:39:42 -0700

    fixed links in contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -21,9 +21,9 @@ export default function ContactPage() {
             <div className="flex flex-col">
                 <div className="text-2xl font-semibold">Contact Links</div>
                 <div className={`${inconsolata.className} flex flex-col pl-3 ml-1.5 border-l border-neutral-500`}>
-                <h1>Email: <Link target="_blank"  href="https://drive.google.com/file/d/1XV6Ljz91bFPV8PJMLyVWi_VY2CSZGk39/view?usp=sharing"  className={`text-red-500 hover:text-red-400`} >thinkpads4all@gmail.com</Link></h1>
-                <h1>Instagram: <Link target="_blank" href="https://drive.google.com/file/d/1zGXOJlG_TFKnUq387PnHKJwu591RFSOA/view?usp=sharing"  className={`text-red-500 hover:text-red-400`}>@thinkpads4all</Link></h1>
-                <h1>LinkedIn: <Link target="_blank"  className={`text-red-500 hover:text-red-400`} href="https://www.canva.com/design/DAGLBugAo08/agVoSioKPwtxXDrG2YloYA/view?utm_content=DAGLBugAo08&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h805b2c19e3" >linkedin.com/company/thinkpads4all/</Link></h1>
+                <h1>Email: <Link target="_blank"  href="mailto:thinkpads4all@gmail.com"  className={`text-red-500 hover:text-red-400`} >thinkpads4all@gmail.com</Link></h1>
+                <h1>Instagram: <Link target="_blank" href="https://www.instagram.com/thinkpads4all"  className={`text-red-500 hover:text-red-400`}>@thinkpads4all</Link></h1>
+                <h1>LinkedIn: <Link target="_blank" href="https://www.linkedin.com/company/thinkpads4all" className={`text-red-500 hover:text-red-400`}  >linkedin.com/company/thinkpads4all/</Link></h1>
 
                 </div>
 
@@ -57,12 +57,12 @@ export default function ContactPage() {
                         <h1 className="">Co-Founder</h1>
                         <div className="flex flex-row space-x-2  items-center justify-center sm:justify-normal">
                             <svg className="w-4 h-4 fill-white" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EmailIcon"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4-8 5-8-5V6l8 5 8-5z"></path></svg>
-                            <p className=""><a target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-opacity" href="mailto:anatulya.nandi@gmail.com">anatulya.nandi@gmail.com</a></p>
+                            <p className=""><a target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-opacity" href="mailto:kxie1416@gmail.com">kxie1416@gmail.com</a></p>
 
                         </div>
-                        <div className="flex flex-row  space-x-2 items-center justify-center sm:justify-normal">
+                        <div className="flex flex-row  space-x-2 items-center justify-center sm:justify-normal">    
                             <svg className="w-4 h-4 fill-white" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LinkedInIcon"><path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"></path></svg>
-                            <p className=""><a target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-opacity" href="https://www.linkedin.com/in/anatulya-nandi-4520a6329/">LinkedIn</a></p>
+                            <p className=""><a target="_blank" className="underline underline-offset-4 hover:text-neutral-200 transition-opacity" href="https://www.linkedin.com/in/kevin-xie-267700231">LinkedIn</a></p>
                         </div>
                     </div>
                     </div>
